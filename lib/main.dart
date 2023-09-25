d6c1014a6fae236473da58583efa7c01244ca330
Shariar Hossain <shariarhossin99@gmail.com>
2023-09-25 16:05:36 +0600

    customize theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:todo/expenses.dart';
 
-void main(){
+void main() {
+  var kColorSceme = ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 2, 156, 200),
+  );
+
   runApp(
-     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorSceme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorSceme.onPrimaryContainer,
+          foregroundColor: kColorSceme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorSceme.primaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorSceme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColorSceme.onSecondaryContainer,
+                fontSize: 20,
+              ),
+            ),
+      ),
       home: const Expenses(),
     ),
   );
--- lib/widgets/widgets_list/expenses_list.dart
+++ lib/widgets/widgets_list/expenses_list.dart
@@ -17,6 +17,12 @@ class ExpensesList extends StatelessWidget {
     return ListView.builder(
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.25),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
         key: ValueKey(expenses[index]),
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
@@ -26,4 +32,3 @@ class ExpensesList extends StatelessWidget {
     );
   }
 }
-
